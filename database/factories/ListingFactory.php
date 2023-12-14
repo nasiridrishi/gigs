b54c82eb68c83b80aef7f130556c7b2f9f28494f
Nasir Idrishi <nasiridrishi@outlook.com>
2023-12-14 11:02:19 +0530

    first commit

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Listing;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,5 @@
+<h1>{{$heading}}</h1>
+@foreach($listings as $listing)
+    <h3>{{$listing['title']}}</h3>
+    <p>{{$listing['description']}}</p>
+@endforeach
